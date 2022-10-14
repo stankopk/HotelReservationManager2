891d37300373a8e06068dd2d35363a4424919567
Stanko Yanev <stanko.yanev@edu.mon.bg>
2022-10-14 12:41:23 +0300

    Add project files.

---
+++ HotelReservationManager/Program.cs
@@ -0,0 +1,23 @@
+﻿using HotelReservationManager.View;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HotelReservationManager
+{
+    internal static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new MainView());
+        }
+    }
+}
